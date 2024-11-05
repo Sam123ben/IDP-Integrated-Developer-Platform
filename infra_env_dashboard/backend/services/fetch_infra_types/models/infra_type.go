ed8c8db115637e5e69a6e88da9e80a03303ecddf
Samyak Rout <samyak.rout@civica.com.au>
2024-11-05 20:49:52 +1100

    Updated the changes

--- infra_env_dashboard/backend/services/fetch_infra_types/models/infra_type.go
+++ infra_env_dashboard/backend/services/fetch_infra_types/models/infra_type.go
@@ -1,8 +1,9 @@
 package models
 
 type InfraType struct {
-	ID   int    `gorm:"primaryKey" json:"id"`
-	Name string `json:"name"`
+	ID       int    `gorm:"primaryKey" json:"id"`
+	Type     string `json:"type"`
+	Sections string `json:"sections"` // Assuming sections is stored as a JSON or string in DB
 }
 
 func (InfraType) TableName() string {
