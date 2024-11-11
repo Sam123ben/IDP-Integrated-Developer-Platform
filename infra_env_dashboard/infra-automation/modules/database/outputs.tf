86d43cff0f8cea130b6a5411196fd2521f5814a2
Samyak Rout <samyak.rout@civica.com.au>
2024-11-12 10:27:20 +1100

    bug: Correct PostgreSQL flexible server outputs
    - Updated `db_name` output to reference `azurerm_postgresql_flexible_server_database.database.name`.
    - Adjusted `db_server_fqdn` to reference `azurerm_postgresql_flexible_server.db_server.fqdn`.
    - Updated `db_server_name` to reference `azurerm_postgresql_flexible_server.db_server.name`.
    
    These changes align the outputs with the flexible PostgreSQL server resource, ensuring the correct attributes are referenced in the Terraform configuration.

--- infra_env_dashboard/infra-automation/modules/database/outputs.tf
+++ infra_env_dashboard/infra-automation/modules/database/outputs.tf
@@ -1,12 +1,14 @@
 output "db_name" {
-  value = azurerm_postgresql_database.database.name
+  description = "Name of the PostgreSQL database"
+  value       = azurerm_postgresql_flexible_server_database.database.name
 }
 
 output "db_server_fqdn" {
-  description = "FQDN of the PostgreSQL server"
-  value       = azurerm_postgresql_server.db_server.fqdn  # Adjust to match your PostgreSQL server resource name
+  description = "FQDN of the PostgreSQL flexible server"
+  value       = azurerm_postgresql_flexible_server.db_server.fqdn
 }
 
 output "db_server_name" {
-  value = azurerm_postgresql_server.db_server.name
+  description = "Name of the PostgreSQL flexible server"
+  value       = azurerm_postgresql_flexible_server.db_server.name
 }
