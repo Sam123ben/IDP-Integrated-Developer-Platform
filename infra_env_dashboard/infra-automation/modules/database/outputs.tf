4bfb1fc0806a09b03d862b331a607331a740669f
Samyak Rout <samyak.rout@civica.com.au>
2024-11-12 00:25:38 +1100

    Fix the db_server_name variable reference

--- infra_env_dashboard/infra-automation/modules/database/outputs.tf
+++ infra_env_dashboard/infra-automation/modules/database/outputs.tf
@@ -6,3 +6,7 @@ output "db_server_fqdn" {
   description = "FQDN of the PostgreSQL server"
   value       = azurerm_postgresql_server.db_server.fqdn  # Adjust to match your PostgreSQL server resource name
 }
+
+output "db_server_name" {
+  value = azurerm_postgresql_server.db_server.name
+}
