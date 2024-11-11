fc6d2c2735918066f9198b937d69d84c3b4f2dcc
Samyak Rout <samyak.rout@civica.com.au>
2024-11-12 00:23:39 +1100

    feat: Add outputs for resource group name and location in Terraform configuration
    - Added output for the name of the created resource group
    - Added output for the location of the created resource group

--- infra_env_dashboard/infra-automation/modules/resource_group/main.tf
+++ infra_env_dashboard/infra-automation/modules/resource_group/main.tf
@@ -1,9 +1,4 @@
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure location where the resource group will be created"
-  type        = string
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
