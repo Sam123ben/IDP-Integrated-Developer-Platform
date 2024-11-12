a6e2a22ec619413c750c478a1816c52b27972528
Samyak Rout <samyak.rout@civica.com.au>
2024-11-12 23:10:26 +1100

    Fixed the outputs for the app services

--- infra_env_dashboard/infra-automation/modules/app/outputs.tf
+++ infra_env_dashboard/infra-automation/modules/app/outputs.tf
@@ -1,7 +1,9 @@
 output "backend_app_url" {
-  value = azurerm_linux_web_app.backend_app.default_site_hostname
+  value = azurerm_linux_web_app.backend_app.default_hostname
+  description = "URL of the backend app service"
 }
 
 output "frontend_app_url" {
-  value = azurerm_linux_web_app.frontend_app.default_site_hostname
+  value = azurerm_linux_web_app.frontend_app.default_hostname
+  description = "URL of the frontend app service"
 }
