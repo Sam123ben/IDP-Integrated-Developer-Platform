48cd3d34553eb366282ebd03b49330b1608f085c
Samyak Rout <samyak.rout@civica.com.au>
2024-11-12 23:08:09 +1100

    Fixed the changes for the output

--- infra_env_dashboard/infra-automation/modules/app/outputs.tf
+++ infra_env_dashboard/infra-automation/modules/app/outputs.tf
@@ -1,7 +1,7 @@
 output "backend_app_url" {
-  value = azurerm_app_service.backend_app.default_site_hostname
+  value = azurerm_linux_web_app.backend_app.default_site_hostname
 }
 
 output "frontend_app_url" {
-  value = azurerm_app_service.frontend_app.default_site_hostname
+  value = azurerm_linux_web_app.frontend_app.default_site_hostname
 }
