5392b2c18101adc79fd2c6cf87ad2c3c494720ff
Samyak Rout <samyak.rout@civica.com.au>
2024-11-13 18:48:11 +1100

    Fixes the outputs for the public_subnet_id

--- infra_env_dashboard/infra-automation/modules/network/outputs.tf
+++ infra_env_dashboard/infra-automation/modules/network/outputs.tf
@@ -2,6 +2,10 @@ output "vnet_id" {
   value = azurerm_virtual_network.vnet.id
 }
 
+output "public_subnet_id" {
+  value = azurerm_subnet.public_subnet.id
+}
+
 output "app_subnet_id" {
   value = azurerm_subnet.app_subnet.id
 }
