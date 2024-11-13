aef294b0717d2621aea4d989597dc4380eb2b93d
Samyak Rout <samyak.rout@civica.com.au>
2024-11-13 19:37:07 +1100

    Fix: Updated the changes to fix the openvpn_custom_database64

--- infra_env_dashboard/infra-automation/modules/openvpn/main.tf
+++ infra_env_dashboard/infra-automation/modules/openvpn/main.tf
@@ -1,10 +1,21 @@
+# Import the base64 encoding function
+locals {
+  openvpn_custom_data = <<-CUSTOM_DATA
+    #cloud-config
+    runcmd:
+      - echo "${var.vm_admin_password}" | passwd ${var.vm_admin_username} --stdin
+      - systemctl enable openvpnas
+      - systemctl start openvpnas
+  CUSTOM_DATA
+}
+
 # Public IP for OpenVPN VM
 resource "azurerm_public_ip" "openvpn_public_ip" {
   name                = "openvpn-public-ip"
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
-  sku                 = "Standard"  # Recommended for VMs with NSGs for improved security
+  sku                 = "Standard"
 
   tags = var.tags
 }
@@ -19,18 +30,18 @@ resource "azurerm_network_interface" "openvpn_nic" {
     name                          = "internal"
     subnet_id                     = var.public_subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.openvpn_public_ip.id  # Attach public IP
+    public_ip_address_id          = azurerm_public_ip.openvpn_public_ip.id
   }
 }
 
 # VM for OpenVPN Access Server
 resource "azurerm_linux_virtual_machine" "openvpn_vm" {
-  name                        = "openvpn-vm"
-  resource_group_name         = var.resource_group_name
-  location                    = var.location
-  size                        = "Standard_B1s"
-  admin_username              = var.vm_admin_username
-  admin_password              = var.vm_admin_password
+  name                            = "openvpn-vm"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.vm_admin_username
+  admin_password                  = var.vm_admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
@@ -44,22 +55,16 @@ resource "azurerm_linux_virtual_machine" "openvpn_vm" {
 
   # OpenVPN Access Server image details
   source_image_reference {
-    publisher = "openvpn"        # OpenVPN Access Server publisher
-    offer     = "openvpnas"      # Offer for OpenVPN Access Server
-    sku       = "openvpnas"      # SKU for OpenVPN Access Server
+    publisher = "openvpn"
+    offer     = "openvpnas"
+    sku       = "openvpnas"
     version   = "latest"
   }
 
   tags = var.tags
 
-  # Cloud-init to start OpenVPN Access Server
-  custom_data = <<-CUSTOM_DATA
-    #cloud-config
-    runcmd:
-      - echo "${var.vm_admin_password}" | passwd ${var.vm_admin_username} --stdin
-      - systemctl enable openvpnas
-      - systemctl start openvpnas
-  CUSTOM_DATA
+  # Base64 encode the custom_data
+  custom_data = base64encode(local.openvpn_custom_data)
 
-  depends_on = [ azurerm_network_interface.openvpn_nic ]
+  depends_on = [azurerm_network_interface.openvpn_nic]
 }
